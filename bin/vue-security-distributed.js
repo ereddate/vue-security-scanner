5fcd066cbc17b8ee0ff45af49fa68c585dacd212
VueSecurityTeam <vue-security@example.com>
2026-01-29 15:00:45 +0800

    更新所有包版本并发布到npm

--- bin/vue-security-distributed.js
+++ bin/vue-security-distributed.js
@@ -8,7 +8,7 @@ const path = require('path');
 const program = require('commander');
 
 program
-  .version('1.5.0')
+  .version('1.5.1')
   .description('Vue Security Scanner - Distributed Mode');
 
 program
